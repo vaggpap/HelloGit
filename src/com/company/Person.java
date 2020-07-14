d5f589ea65f97bdd64cfffc9b7c6fced68fcb3c0
vagelis <vagospap@windowslive.com>
2020-07-14 12:59:15 +0300

    feature a

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,18 +3,28 @@ package com.company;
 public class Person {
     private String firstname;
     private String lastname;
+    private int age;
 
-    public void setFirstname(String firstname){
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setFirstname(String firstname) {
         this.firstname = firstname;
     }
-    public void setLastname(String lastname){
+
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    public String getFirstname(){
+    public String getFirstname() {
         return firstname;
     }
-    public String getLastname(){
+
+    public String getLastname() {
         return lastname;
     }
 }
