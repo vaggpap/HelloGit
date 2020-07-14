76e12d58c62dbbb63e05fe9edffda3a3e8cc11e1
vagelis <vagospap@windowslive.com>
2020-07-14 12:20:10 +0300

    Add Person class

---
+++ src/com/company/Person.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Person {
+    private String firstname;
+
+    public void setFirstname(String firstname){
+        this.firstname = firstname
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+}
