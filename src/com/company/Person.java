cf5278f6bac05170829f88427211e6922201b3d7
vagelis <vagospap@windowslive.com>
2020-07-14 12:42:06 +0300

    add last name property

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -2,12 +2,19 @@ package com.company;
 
 public class Person {
     private String firstname;
+    private String lastname;
 
     public void setFirstname(String firstname){
         this.firstname = firstname;
     }
+    public void setLastname(String lastname){
+        this.lastname = lastname;
+    }
 
     public String getFirstname(){
         return firstname;
+    }
+    public String getLastname(){
+        return lastname;
     }
 }
